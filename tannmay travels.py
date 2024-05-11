eea357dbf057bbd9a78b085ed4d858da09b1823f
Tannmay Khandelwal <94522751+tannmaycoding@users.noreply.github.com>
2024-05-11 17:56:19 +0530

    Update tannmay travels.py

--- tannmay travels.py
+++ tannmay travels.py
@@ -1,12 +1,5 @@
 """
 This is a form in Tkinter.
-Modules used:
-    Everything from tkinter,
-    tkinter.messagebox as tmsg
-
-Functions made:
-    rate: Shows a dialog box to rate app experience,
-    getValues: Gives values from form and appends it in txt file.
 """
 
 from tkinter import *
